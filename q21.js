d0ae4e8db3221eddf3154da091e0a6e24261d37f
Imran latif <159417772+Imranlat@users.noreply.github.com>
2024-03-11 20:53:59 +0500

    Add files via upload

---
+++ q21.js
@@ -0,0 +1,12 @@
+"use strict";
+//21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let person1 = {
+    name: "Murad",
+    age: 25,
+    city: "New York"
+};
+let person2 = {
+    name: "Tabish",
+    age: 30,
+    city: "London"
+};
---
+++ q21.ts
@@ -0,0 +1,19 @@
+//21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+interface Person {
+    name: string;
+    age: number;
+    city: string;
+}
+
+let person1: Person = {
+    name: "Murad",
+    age: 25,
+    city: "New York"
+};
+
+let person2: Person = {
+    name: "Tabish",
+    age: 30,
+    city: "London"
+};
